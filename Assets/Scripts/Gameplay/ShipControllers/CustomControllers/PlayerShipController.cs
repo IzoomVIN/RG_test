56ceeaaf779759b0b7a265d80d0c525ddba069ee
Vinogradov Vyacheslav <Slava147148@gmail.com>
2021-10-05 20:53:43 +0300

    initial

---
+++ Assets/Scripts/Gameplay/ShipControllers/CustomControllers/PlayerShipController.cs
@@ -0,0 +1,23 @@
+﻿using Gameplay.ShipSystems;
+using UnityEngine;
+
+namespace Gameplay.ShipControllers.CustomControllers
+{
+    public class PlayerShipController : ShipController
+    {
+        [SerializeField] private KeyCode key;
+        
+        protected override void ProcessHandling(MovementSystem movementSystem)
+        {
+            movementSystem.LateralMovement(Input.GetAxis("Horizontal") * Time.deltaTime);
+        }
+
+        protected override void ProcessFire(WeaponSystem fireSystem)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                fireSystem.TriggerFire();
+            }
+        }
+    }
+}
